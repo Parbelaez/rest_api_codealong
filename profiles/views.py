0a86ad0d2aaf7d72b2515b6138268b2847527dd4
Paulo Arbeláez <pauloarbelaez@gmail.com>
2023-11-07 09:27:37 +0000

    Create the serializers and add permissions

--- profiles/views.py
+++ profiles/views.py
@@ -4,30 +4,40 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
+from rest_api_codealong.permissions import IsOwnerOrReadOnly
 
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
+        serializer = ProfileSerializer(
+            profiles, many=True, context={'request' : request}
+            )
         return Response(serializer.data)
 
 class ProfileDetails(APIView):
     serializer_class = ProfileSerializer
+    permission_classes = [
+        IsOwnerOrReadOnly
+    ]
+
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist as e:
             raise Http404 from e
 
     def get(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile)
+        serializer = ProfileSerializer(profile, context={'request' : request})
         return Response(serializer.data)
 
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(
+            profile, data=request.data, context={'request' : request}
+            )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
