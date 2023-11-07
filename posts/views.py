7b0e55d334d83630f1fbaa55e79b408ba75c126e
Paulo Arbeláez <pauloarbelaez@gmail.com>
2023-11-07 20:55:11 +0000

    Finish CRUD for post: update and delete

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Post
 from .serializers import PostSerializer
+from rest_api_codealong.permissions import IsOwnerOrReadOnly
 
 
 class PostList(APIView):
@@ -29,4 +31,40 @@ class PostList(APIView):
             )
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+class PostDetails(APIView):
+    serializer_class = PostSerializer
+    permission_classes = [
+        IsOwnerOrReadOnly
+    ]
+
+    def get_object(self, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return post
+        except Post.DoesNotExist as e:
+            raise Http404 from e
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, context={'request' : request})
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, data=request.data, context={'request' : request}
+            )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
         )
