448f001f0327332ada9adc9dcf30175dc210a879
Paulo Arbeláez <pauloarbelaez@gmail.com>
2023-11-12 11:37:05 +0000

    Add the likes and comments count/filter

--- posts/views.py
+++ posts/views.py
@@ -1,21 +1,35 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
 from .models import Post
 from .serializers import PostSerializer
 from rest_api_codealong.permissions import IsOwnerOrReadOnly
 
 
 class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True),
+    ).order_by('-created_at')
     serializer_class = PostSerializer
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
+    ]
+    filter_backends = [
+        filters.OrderingFilter,
+    ]
+    ordering_fields = [
+        'likes_count',
+        'comments_count',
     ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 class PostDetails(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True),
+    ).order_by('-created_at')
     serializer_class = PostSerializer
     permission_classes = [
         IsOwnerOrReadOnly
