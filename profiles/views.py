6a4f5934a3aefbe4fab4c0d1f2b8f708fa82f9e7
Paulo Arbeláez <pauloarbelaez@gmail.com>
2023-11-12 09:29:02 +0000

    Create the filters and refadctoring for  profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,44 +1,43 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.db.models import Count
+from rest_framework import generics, filters
+from rest_api_codealong.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
-from rest_api_codealong.permissions import IsOwnerOrReadOnly
 
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles, many=True, context={'request' : request}
-            )
-        return Response(serializer.data)
 
-class ProfileDetails(APIView):
+class ProfileList(generics.ListAPIView):
+    """
+    List all profiles.
+    No create view as profile creation is handled by the UserCreate view.
+    """
+
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+    ).order_by('-created_at')
+    serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter,
+        ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+        ]
+
+class ProfileDetails(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve, update or delete a profile instance.
+    """
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
     permission_classes = [
         IsOwnerOrReadOnly
     ]
-
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist as e:
-            raise Http404 from e
-
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request' : request})
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, data=request.data, context={'request' : request}
-            )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
